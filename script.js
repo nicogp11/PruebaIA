88d5d0b6f7d26e8016a29242fa4335fce6b62b01
Nicolás Gonzalez <110434893+nicogp11@users.noreply.github.com>
2024-08-18 17:45:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var redNeuronal = {neurona}
+
+function crearPesos(cantPesos,min,max){
+    let pesos = {};
+
+    for (let i = 1; i <= cantPesos; i++) {
+        pesos[`peso${i}`] = Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    return pesos;
+}
+
+function crearNeurona(capa,idNeurona,cantPesos,min,max){
+let neurona = {}
+
+neurona.capa = capa;
+neurona.idNeurona = idNeurona;
+neurona.sesgo = Math.floor(Math.random() * (max - min + 1) + min);
+neurona.pesos = crearPesos(cantPesos,min,max)
+
+
+}
+
+
+
+function funcionActivacion(sumatoria) {
+    
+}
